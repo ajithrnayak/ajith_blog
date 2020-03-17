412d16b4d6eec4108f4495ea330d3897b63a808f
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-17 20:10:35 +0530

    Added additional plugins needed for `gatsby-transformer-remark`

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+'use strict';
+
+require("prismjs/themes/prism-okaidia.css")
--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,28 @@ module.exports = {
       path: `${__dirname}/content`,
       name: 'pages'
     }
+  },
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            maxWidth: 960,
+            withWebp: true,
+            ignoreFileExtensions: [],
+          }
+        },
+        {
+          resolve: 'gatsby-remark-responsive-iframe',
+          options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
+        },
+        'gatsby-remark-autolink-headers',
+        'gatsby-remark-prismjs',
+        'gatsby-remark-copy-linked-files',
+        'gatsby-remark-smartypants'
+      ]
+    }
   }]
 }
