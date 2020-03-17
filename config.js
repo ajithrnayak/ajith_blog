9d86d7498eb1fe78f9489bd7b3099c25d42923ec
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-17 18:57:45 +0530

    Added a config file for site metadata.

---
+++ config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  url: 'https://ajith.blog',
+  pathPrefix: '/',
+  title: "Ajith's blog",
+  subtitle: 'I occasionally blog here. Checkout my website.',
+  copyright: 'Designed & developed by Ajith R Nayak',
+  disqusShortname: 'ajithrnayak',
+  postsPerPage: 10,
+  googleAnalyticsId: 'UA-43294206-2',
+  website: 'https://ajith.xyz',
+  author: {
+    name: 'Ajith R Nayak',
+    bio: 'Swift/Objective-C developer since iOS 6.'
+  }
+};
