d8896d6bd59951c61020692d0dd365cac70bae01
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-17 19:07:07 +0530

    Updated site metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,24 @@
 /**
- * Configure your Gatsby site with this file.
- *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+'use strict';
+
+const siteConfig = require('./config.js');
+
 module.exports = {
-  plugins: [`gatsby-plugin-sass`,
+  // @see https://www.gatsbyjs.org/docs/path-prefix/
+  pathPrefix: siteConfig.pathPrefix,
+  siteMetadata: {
+    url: siteConfig.url,
+    title: siteConfig.title,
+    subtitle: siteConfig.subtitle,
+    copyright: siteConfig.copyright,
+    disqusShortname: siteConfig.disqusShortname,
+    author: siteConfig.author
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
   {
     resolve: 'gatsby-source-filesystem',
     options: {
