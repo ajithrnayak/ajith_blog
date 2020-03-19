4543ad9ff6310c52edd48dd60e193f1765da153e
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-19 17:59:58 +0530

    Implemented graphql query hook to get site metadata.

---
+++ src/query/use-site-metadata.js
@@ -0,0 +1,30 @@
+/**
+ * Read more about hooks 
+ * https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import { useStaticQuery, graphql } from 'gatsby';
+
+export const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            author {
+              name
+              bio
+            }
+            url
+            title
+            subtitle
+            copyright
+            disqusShortname
+          }
+        }
+      }
+    `
+  )
+
+  return site.siteMetadata
+};
