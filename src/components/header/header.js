04dc33500a6479031601e3ebeddbf16de1ae3b3a
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-19 17:57:39 +0530

    Implemented a header component with basic styling

---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TitleAndCaption from '../titleAndCaption/titleAndCaption';
+import { useSiteMetadata } from '../../query/use-site-metadata'
+import styles from './header.module.scss';
+
+
+const Header = () => {
+  const data = useSiteMetadata();
+
+  return (
+    <div className={styles.header}>
+    <TitleAndCaption data={data}/>
+    </div>
+  );
+};
+
+export default Header;
