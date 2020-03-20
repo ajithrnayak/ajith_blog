45f28470f0d6f2f8d838481714c93e5757f7722c
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-20 19:31:40 +0530

    Code formatting using Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,26 @@
-import React from "react";
-import { graphql } from 'gatsby';
-import Layout from "../components/layout/layout";
-import Posts from "../components/posts/posts";
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout/layout"
+import Posts from "../components/posts/posts"
 
 const BlogIndex = ({ data, location }) => {
   const { edges } = data.allMarkdownRemark
   return (
     <Layout>
-      <Posts posts={edges}/>
+      <Posts posts={edges} />
     </Layout>
-  );
-};
+  )
+}
 
-export default BlogIndex;
-
+export default BlogIndex
 
 export const postsQuery = graphql`
-  query PostsQuery 
-  {
-    allMarkdownRemark 
-    {
-      edges 
-      {
-        node 
-        {
+  query PostsQuery {
+    allMarkdownRemark {
+      edges {
+        node {
           timeToRead
-          frontmatter 
-          {
+          frontmatter {
             category
             date(formatString: "MMMM DD, YYYY")
             excerpt
