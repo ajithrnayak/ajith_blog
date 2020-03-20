80cc0cef7ea6acbdd5ae22cea98e5098c2b03dbf
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-20 19:14:33 +0530

    Code formatting using prettier.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-'use strict';
+"use strict"
 
-const siteConfig = require('./config.js');
+const siteConfig = require("./config.js")
 
 module.exports = {
   // @see https://www.gatsbyjs.org/docs/path-prefix/
@@ -16,38 +16,39 @@ module.exports = {
     copyright: siteConfig.copyright,
     disqusShortname: siteConfig.disqusShortname,
     author: siteConfig.author,
-    website: siteConfig.website
+    website: siteConfig.website,
   },
   plugins: [
     `gatsby-plugin-sass`,
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      path: `${__dirname}/content`,
-      name: 'pages'
-    }
-  },
-  {
-    resolve: 'gatsby-transformer-remark',
-    options: {
-      plugins: [
-        {
-          resolve: 'gatsby-remark-images',
-          options: {
-            maxWidth: 960,
-            withWebp: true,
-            ignoreFileExtensions: [],
-          }
-        },
-        {
-          resolve: 'gatsby-remark-responsive-iframe',
-          options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
-        },
-        'gatsby-remark-autolink-headers',
-        'gatsby-remark-prismjs',
-        'gatsby-remark-copy-linked-files',
-        'gatsby-remark-smartypants'
-      ]
-    }
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/content`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 960,
+              withWebp: true,
+              ignoreFileExtensions: [],
+            },
+          },
+          {
+            resolve: "gatsby-remark-responsive-iframe",
+            options: { wrapperStyle: "margin-bottom: 1.0725rem" },
+          },
+          "gatsby-remark-autolink-headers",
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+        ],
+      },
+    },
+  ],
 }
