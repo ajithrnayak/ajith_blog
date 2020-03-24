0e0c1a69a7cb9cee4e6f142eee3264d92359e87a
Ajith R Nayak <ajithrnayak@icloud.com>
2020-03-25 01:10:09 +0530

    Implemented a component for pagination with minimal styling.

---
+++ src/components/pagination/pagination.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "gatsby"
+import styles from "./pagination.module.scss"
+
+const Pagination = ({ previous, next }) => {
+  return (
+    <div>
+      <nav>
+        <ul className={styles.pagination}>
+          <li>
+            {previous && (
+              <Link to={previous.frontmatter.path} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.frontmatter.path} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Pagination
