154faeab67f876ebcadd59494f37381d952a1dc4
Ajith R Nayak <ajithrnayak@icloud.com>
2020-04-27 10:51:53 +0530

    Use `gatsby-source-filesystem` to read font files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "fonts",
+        path: `${__dirname}/static/fonts`
+      }
+    },
+    {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
