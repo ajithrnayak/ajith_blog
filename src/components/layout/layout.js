24aa3959c58327d3eb80b3de490e7f46a9e0f596
Ajith R Nayak <ajithrnayak@icloud.com>
2020-04-28 23:00:20 +0530

    Added left and right padding for layout component

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,17 +1,16 @@
-import React from "react";
-import Header from '../header/header';
-import Footer from '../footer/footer';
-import styles from './layout.module.scss';
+import React from "react"
+import Header from "../header/header"
+import Footer from "../footer/footer"
+import styles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
-
   return (
     <div className={styles.layout}>
       <Header />
       <main>{children}</main>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
