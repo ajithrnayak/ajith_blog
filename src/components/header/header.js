106b68bd48df9085019a3641db0138e07ccfc361
Ajith R Nayak <ajithrnayak@icloud.com>
2020-05-01 02:11:39 +0530

    Fixes a issue with linking title and caption component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,16 @@
-import React from "react";
-import TitleAndCaption from '../titleAndCaption/titleAndCaption';
-import { useSiteMetadata } from '../../hooks/use-site-metadata';
-import styles from './header.module.scss';
-
+import React from "react"
+import TitleAndCaption from "../title-and-caption/title-and-caption"
+import { useSiteMetadata } from "../../hooks/use-site-metadata"
+import styles from "./header.module.scss"
 
 const Header = () => {
-  const data = useSiteMetadata();
+  const data = useSiteMetadata()
 
   return (
     <header className={styles.header}>
-    <TitleAndCaption data={data}/>
+      <TitleAndCaption data={data} />
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
