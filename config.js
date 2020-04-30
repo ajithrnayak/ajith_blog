050aa42bebb3964b945270b7b50a70dc878023dd
Ajith R Nayak <ajithrnayak@icloud.com>
2020-05-01 01:28:06 +0530

    Added desciption about the site for meta tags.

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
-'use strict';
+"use strict"
 
 module.exports = {
-  url: 'https://ajith.blog',
-  pathPrefix: '/',
+  url: "https://ajith.blog",
+  pathPrefix: "/",
   title: "Ajith's blog",
-  subtitle: 'I occasionally blog here.',
-  copyright: 'Designed & developed by Ajith R Nayak',
-  disqusShortname: 'ajithrnayak',
+  description:
+    "I build apps and sometimes write about the observations and frustrations at work here.",
+  subtitle: "I occasionally blog here.",
+  copyright: "Designed & developed by Ajith R Nayak",
+  disqusShortname: "ajithrnayak",
   postsPerPage: 10,
-  googleAnalyticsId: 'UA-43294206-2',
-  website: 'https://ajith.xyz',
-  sourceCode: 'http://github.com/ajithrnayak/ajith_blog',
+  googleAnalyticsId: "UA-43294206-2",
+  website: "https://ajith.xyz",
+  sourceCode: "http://github.com/ajithrnayak/ajith_blog",
   author: {
-    name: 'Ajith R Nayak',
-    bio: 'Swift/Objective-C developer since iOS 6.'
-  }
-};
+    name: "Ajith R Nayak",
+    bio: "Swift/Objective-C developer since iOS 6.",
+  },
+}
