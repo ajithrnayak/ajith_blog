9f793f94199e876d5d1492babe2c9c25458b4cb4
Ajith R Nayak <ajithrnayak@icloud.com>
2020-05-02 02:29:55 +0530

    Fixed inline issues with caption

--- src/components/title-and-caption/title-and-caption.js
+++ src/components/title-and-caption/title-and-caption.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React from "react"
 import { Link } from "gatsby"
-import styles from './title-and-caption.module.scss';
+import styles from "./title-and-caption.module.scss"
 
-const TitleAndCaption = ({data}) => {
+const TitleAndCaption = ({ data }) => {
   const { title, subtitle, website } = data
 
   return (
     <div className={styles.titleAndCaption}>
-      <Link className={styles.titleLink} to={`/`}>
-      <h2 className={styles.blogTitle}>{title}</h2>
-      </Link>
+      <h2 className={styles.blogTitle}>
+        <Link className={styles.titleLink} to={`/`}>
+          {title}
+        </Link>
+      </h2>
       <div className={styles.blogCaption}>
-      <p>{subtitle}</p>
-      <p className={styles.website}>Checkout my <a href={website}>website.</a></p>
+        <p>
+          {subtitle}
+          <span className={styles.website}>
+            Checkout my <a href={website}>website.</a>
+          </span>
+        </p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TitleAndCaption;
+export default TitleAndCaption
