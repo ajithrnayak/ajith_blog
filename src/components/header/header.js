6a61ba082fd7a3ae249a91ee67ba4bb3b6b6c994
Ajith R Nayak <ajithrnayak@icloud.com>
2020-05-06 02:39:34 +0530

    Integrated component to toggle theme

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,16 @@
 import React from "react"
 import TitleAndCaption from "../title-and-caption/title-and-caption"
+import ThemeToggler from "../theme-toggler/theme-toggler"
 import { useSiteMetadata } from "../../hooks/use-site-metadata"
 import styles from "./header.module.scss"
 
-const Header = () => {
+const Header = ({isDark, themeToggleHandler}) => {
   const data = useSiteMetadata()
 
   return (
     <header className={styles.header}>
       <TitleAndCaption data={data} />
+      <ThemeToggler isDark={isDark} toggle={themeToggleHandler}/>
     </header>
   )
 }
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 import Header from "../header/header"
 import Footer from "../footer/footer"
 import styles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
+  const [selectedMode, setSelectedMode] = useState('light')
+  const isDark = selectedMode === `dark`
+  let themeToggleHandler = () => {
+    if (selectedMode === 'dark') {
+      setSelectedMode('light')
+    } 
+    else {
+      setSelectedMode('dark')
+    }
+  }
+
   return (
     <div className={styles.layout}>
-      <Header />
+      <Header isDark={isDark} themeToggleHandler={themeToggleHandler}/>
       <main>{children}</main>
       <Footer />
     </div>
