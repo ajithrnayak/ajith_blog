f895a526724ae01d55e9ec2456ce8dd5f31ed526
Ajith R Nayak <ajithrnayak@icloud.com>
2020-05-09 01:04:12 +0530

    Implemented svg file size optimiser

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,6 +100,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-optimize-svgs',
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
