d2a29867be3faa07117fb5b92e5d9586d83d47fc
Ajith Renjala <ajithrnayak@icloud.com>
2022-09-02 03:52:54 +0530

    Fixes few warnings

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import styles from "../styles/404/404.module.scss"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 const NotFoundPage = () => {
   return (
     <Layout>
-      <SEO title="404: Not Found" />
+      <Seo title="404: Not Found" />
       <div className={styles.not_found}>
         <h1>404</h1>
       </div>
