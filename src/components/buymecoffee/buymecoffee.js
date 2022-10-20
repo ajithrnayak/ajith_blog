a47f127acb49372dca41a48b112a2ac0632bf33c
Ajith Renjala <ajithrnayak@icloud.com>
2022-10-20 11:24:07 +0530

    Show me the glitters

--- src/components/buymecoffee/buymecoffee.js
+++ src/components/buymecoffee/buymecoffee.js
@@ -7,7 +7,7 @@ const BuyMeCoffee = () => {
     "https://cdn.buymeacoffee.com/buttons/default-orange.png"
   return (
     <div className={styles.buymecoffee}>
-      <p>Thanks for reading!<br/>If you found my work helpful, buy me a cup of coffee! I will appreciate it a lot.</p>
+      <p>Thanks for reading!✨<br/>If you found my work helpful, buy me a cup of coffee! I will appreciate it a lot.</p>
       <a href={pagelink} target="_blank" rel="noopener noreferrer">
         <img src={buymeacoffeeIcon} alt={"Buy Me A Coffee"}></img>
       </a>
